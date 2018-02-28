8b1c41e9958c9be030712ed009f453956ae6c386
ThakurPriyanka <aroma.priyanka@gmail.com>
2018-02-28 10:02:51 +0530

    logger level is off

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -7,17 +7,17 @@ import org.apache.log4j.{Level, Logger}
 
 object Application {
   def main(args: Array[String]): Unit ={
-//    Logger.getLogger("org".setLevel(Level.OFF))
+    Logger.getLogger("org").setLevel(Level.OFF)
     val log = Logger.getLogger(this.getClass)
 
     val operationObj = new Operation
     val spark = SparkConfigrationFile.getSparkSession()
-//    log.info(s"${operationObj.readCsvFile(spark).show()}")
-//    log.info(s"${operationObj.homeTeamTotalMatch(spark).show}")
+    log.info(s"${operationObj.readCsvFile(spark).show()}")
+    log.info(s"${operationObj.homeTeamTotalMatch(spark).show}")
     log.info(s"${operationObj.getTopTenWinningPercent(spark).show}")
-//    log.info(s"${operationObj.convertDataFrameToDataSet(spark).show}")
-//    log.info(s"${operationObj.totalNumberOfTeam(spark).show}")
-//    log.info(s"${operationObj.highestWinTeam(spark).show}")
+    log.info(s"${operationObj.convertDataFrameToDataSet(spark).show}")
+    log.info(s"${operationObj.totalNumberOfTeam(spark).show}")
+    log.info(s"${operationObj.highestWinTeam(spark).show}")
   }
 
 }
